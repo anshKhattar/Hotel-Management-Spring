b3c930a7d796806bc0ba2e109dd60f9df46dd185
anshKhattar <akhattar@tekion.com>
2023-02-02 11:41:01 +0530

    this is my first commit

---
+++ src/test/java/com/hotel/management/HotelManagement/HotelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.management.HotelManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
